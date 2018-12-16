89e4b08259e0eb153071dcfdf46131415ea5e624
Karoline Lüthje <Karoline@Karolines-MacBook-Pro-4.local>
2018-12-16 23:32:59 +0100

    responsiv på alle sider undtagen galleri/værker, påbegyndt hover på forside

--- script.js
+++ script.js
@@ -11,6 +11,32 @@ function sidenVises() {
     console.log("siden vises");
     document.querySelector(".menubutton").addEventListener("click", trykPaaMenubutton);
 
+    if (window.innerWidth >= 1050) {
+        document.querySelector("nav").classList.remove("hidden");
+    }
+
+    if (id == null) {
+        console.log("null virker");
+        document.querySelector(".menupunkt_forside a").classList.add("menu_aktiv");
+    }
+    if (id == 1) {
+        console.log("1 virker");
+        document.querySelector(".menupunkt_forside a").classList.add("menu_aktiv");
+    }
+    if (id == 2) {
+        console.log("2 virker");
+        document.querySelector(".menupunkt_galleri a").classList.add("menu_aktiv");
+    }
+    if (id == 3) {
+        console.log("3 virker");
+        document.querySelector(".menupunkt_kunstneren a").classList.add("menu_aktiv");
+    }
+
+    if (id == 4) {
+        console.log("4 virker");
+        document.querySelector(".menupunkt_kontakt a").classList.add("menu_aktiv");
+    }
+
 
     function trykPaaMenubutton() {
         console.log("der er trykket på menu");
@@ -18,30 +44,6 @@ function sidenVises() {
         document.querySelector("nav").classList.toggle("hidden");
         //skift knappen frem og tilbage mellem kryds
         document.querySelector(".menubutton").classList.toggle("kryds");
-
-        //        document.querySelector("body").classList.toggle("overflow");
-
-        if (id == null) {
-            console.log("null virker");
-            document.querySelector(".menupunkt_forside a").classList.add("menu_aktiv");
-        }
-        if (id == 1) {
-            console.log("1 virker");
-            document.querySelector(".menupunkt_forside a").classList.add("menu_aktiv");
-        }
-        if (id == 2) {
-            console.log("2 virker");
-            document.querySelector(".menupunkt_galleri a").classList.add("menu_aktiv");
-        }
-        if (id == 3) {
-            console.log("3 virker");
-            document.querySelector(".menupunkt_kunstneren a").classList.add("menu_aktiv");
-        }
-
-        if (id == 4) {
-            console.log("4 virker");
-            document.querySelector(".menupunkt_kontakt a").classList.add("menu_aktiv");
-        }
 
 
         document.querySelector(".box").classList.toggle("none");
