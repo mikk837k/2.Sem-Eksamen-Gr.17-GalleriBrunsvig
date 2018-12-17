59b5d9a653765dada3d472e1ab27aa5103841355
Shawna Mercano <bruger@laptop-9v6b3a1v.kea.local>
2018-12-17 10:57:12 +0100

    Diverse hover-detaljer //Karoline

--- script.js
+++ script.js
@@ -46,6 +46,29 @@ function sidenVises() {
         document.querySelector(".menubutton").classList.toggle("kryds");
 
 
+        if (id == null) {
+            console.log("null virker");
+            document.querySelector(".index_container").classList.toggle("kanIkkeKlikke");
+        }
+        if (id == 1) {
+            console.log("1 virker");
+            document.querySelector(".index_container").classList.toggle("kanIkkeKlikke");
+        }
+        if (id == 2) {
+            console.log("2 virker");
+            document.querySelector(".galleri_container").classList.toggle("kanIkkeKlikke");
+        }
+        if (id == 3) {
+            console.log("3 virker");
+            document.querySelector(".kunstneren_container").classList.toggle("kanIkkeKlikke");
+        }
+
+        if (id == 4) {
+            console.log("4 virker");
+            document.querySelector(".kontakt_container").classList.toggle("kanIkkeKlikke");
+        }
+
+
         document.querySelector(".box").classList.toggle("none");
         window.onclick = function (event) {
             if (event.target == document.querySelector(".box")) {
